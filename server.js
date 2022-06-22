87e7c33bfda163ff4d9800687d55e13d35de4077
Falashade Greene <falashadegreene@gmail.com>
2022-06-22 19:50:43 -0400

    completed back-end server code

--- server.js
+++ server.js
@@ -1,65 +1,39 @@
 'use strict';
 
-console.log('our first server');
 
-const express = require('express');
+
 require('dotenv').config();
-let data = require('./data/weather.json');
-
-//const cors = require('cors');
-
-
+const express = require('express');
+const cors = require('cors');
 const app = express();
-//app.use(cors());
+const weather = require('./data/weather.json');
+app.use(cors());
+
 
 
 const PORT = process.env.PORT || 3002;
 
-
-
-
-app.get('/', (request, response) => {
-  response.send('hello from our server!');
-})
-
-app.get('/hello', (request, response) => {
-  console.log(request.query.name);
-  let name = request.query.name;
-  let lastName = request.query.lastName;
-  // let fullName = `${name} ${lastName}`;
-  response.send(`Hello ${name} ${lastName}!`);
-});
-
-app.get('/weather', (request, response) => {
-  try {
-    let weatherFromRequest = request.query.weather;
-    console.log(weatherFromRequest);
-    let dataToSearch = data.find(search => search.weather === weatherFromRequest);
-    let dataToSend = new Pet(dataToGroom);
-    response.send(dataToSend);
-  } catch (error) {
-   
-    next(error);
-    
+class Forcast {
+  constructer(weatherObject){
+    this.date = weatherObject.date;
+    this.description = weatherObject.weather.description;
   }
-});
-
-app.get('*', (request, response) => {
-  response.send('The thing you are looking for doesn\'t exist');
-});
-
-
-  //class {
-  //constructor(petObject) {
-    //this.name = petObject.name;
-    //this.breed = petObject.breed;
-  //}
 }
 
 
-app.use((error, request, response, next) => {
-  response.status(500).send(error.message);
-}) 
+app.get('/weather ', (request, response) => {
+  const searchQuery = request.query.searchQuery;
+  console.log(searchQuery);
+  let searchResult = weather.find(object => object.city_name.toLowerCase() === searchQuery.toLowerCase());
+
+  const result = searchResult.data.map(dayObj => new Forcast(dayObj));
+  response.status(200).send(result);
+});
+
+
+app.get('*', (request, response) => {
+  response.send('Does Not Compute');
+});
 
 
 
