2ac4a5b230070773bdbc8b308c924372f4550993
Falashade Greene <falashadegreene@gmail.com>
2022-06-23 00:03:17 -0400

    test

--- server.js
+++ server.js
@@ -13,20 +13,21 @@ app.use(cors());
 
 const PORT = process.env.PORT || 3002;
 
-class Forcast {
-  constructer(weatherObject){
-    this.date = weatherObject.date;
-    this.description = weatherObject.weather.description;
-  }
+function Forcast(weatherObject) {
+  //constructer(weatherObject){
+  this.date = weatherObject.datetime;
+  this.description = weatherObject.weather.description;
+  //}
 }
 
 
-app.get('/weather ', (request, response) => {
+app.get('/weather', (request, response) => {
   const searchQuery = request.query.searchQuery;
-  console.log(searchQuery);
+  //console.log(searchQuery);
   let searchResult = weather.find(object => object.city_name.toLowerCase() === searchQuery.toLowerCase());
-
+  //console.log(searchResult);
   const result = searchResult.data.map(dayObj => new Forcast(dayObj));
+  console.log(result);
   response.status(200).send(result);
 });
 
