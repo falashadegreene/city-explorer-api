80f011344f85c287fc1ed75a9707885567f7eb6b
Falashade Greene <falashadegreene@gmail.com>
2022-07-04 22:31:25 -0400

    added cache and two additinal modules for cache

---
+++ modules/weather1.js
@@ -0,0 +1,42 @@
+'use strict';
+
+let cache = require('./cache.js');
+const axios = require('axios');
+module.exports = getWeather;
+
+async function getWeather(request, response) {
+  const{lat, lon} = request.query;
+  const key = 'weather-' + lat + lon;
+  const url = `http://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lang=en&lat=${lat}&lon=${lon}`;
+  console.log(url);
+  if (cache[key] && Date.now() - cache[key].timestamp < 50000) {
+    console.log('Cache in!');
+  } else {
+    console.log('Cache out!');
+    cache[key]={};
+    cache[key].timestamp = Date.now();
+    cache[key].data = await axios.get(url).then(response => parseWeather(response.data));
+  }
+  console.log(cache[key].data);
+  response.send(cache[key].data);
+}
+
+function parseWeather(dataToGroom) {
+  console.log(dataToGroom);
+  try {
+    const dataToSend = dataToGroom.data.map(day => {
+      return new Forecast(day);
+    });
+    return Promise.resolve(dataToSend);
+  } catch (e) {
+    return Promise.reject(e);
+  }
+}
+
+class Forecast {
+  constructor(ForecastObj) {
+    this.forecast = ForecastObj.weather.description;
+    this.time = ForecastObj.datetime;
+  }
+}
+
